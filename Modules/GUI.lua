e8c57c4d7eb8e014f6803ab70afacfb387e4a476
SeregaRQ <sergeydekker000@gmail.com>
2026-09-19 11:11:00 +0500

    Update GUI.lua

--- Modules/GUI.lua
+++ Modules/GUI.lua
@@ -1,11 +1,1 @@
-local GUI = {
-    loaded = false
-}
 
-function GUI.init()
-    -- Initialize the interface before any other module is loaded.
-    GUI.loaded = true
-    print("GUI loaded")
-end
-
-return GUI
