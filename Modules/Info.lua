c5f132e7ca9ae6a669ca6b97cc6daa1bc02a953d
SeregaRQ <sergeydekker000@gmail.com>
2026-09-19 11:11:07 +0500

    Update Info.lua

--- Modules/Info.lua
+++ Modules/Info.lua
@@ -1,13 +1,1 @@
-local Info = {
-    name = "DeltaSC",
-    version = "0.1.0",
-    description = "A modular Lua application scaffold."
-}
 
-function Info.init()
-    if Info.version == nil then
-        error("DeltaSC version is not configured")
-    end
-end
-
-return Info
