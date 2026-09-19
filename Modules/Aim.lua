4a2708341d3f622d1b9322714fe8941543ce52e4
SeregaRQ <sergeydekker000@gmail.com>
2026-09-19 11:09:10 +0500

    Update Aim.lua

--- Modules/Aim.lua
+++ Modules/Aim.lua
@@ -1,8 +1,1 @@
-local Aim = {}
 
-function Aim.init()
-    -- Intentionally empty safe extension point.
-    -- Add only permitted targeting or interaction logic for your own application.
-end
-
-return Aim
